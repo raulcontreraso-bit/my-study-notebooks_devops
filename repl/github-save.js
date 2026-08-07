8f7b9b0b61a61bd383dba7c828c0407f93d12496
raulcontreraso-bit <raulcontreraso@gmail.com>
2026-08-07 16:49:29 +0200

    Add function to save notebook to GitHub

--- repl/github-save.js
+++ repl/github-save.js
@@ -1,1 +1,20 @@
+async function saveNotebookToGithub(filename, notebook) {
 
+  const response = await fetch(
+    "https://jupyterlite-sync.raulcontreraso.workers.dev/save-notebook",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        filename,
+        content: notebook
+      })
+    }
+  );
+
+  return await response.json();
+}
+
+window.saveNotebookToGithub = saveNotebookToGithub;
